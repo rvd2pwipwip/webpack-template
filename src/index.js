46072f9dedb3e1e343b466b0dc901266fdb80fc6
Herve Desrosiers <herve_desrosiers@hotmail.com>
2024-08-16 08:46:45 -0400

    renamed logo symbol and assigned generated element with new Image() instead of document.createElement('img')

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './style.css';
 import jsLogo from './img/javascript-1.svg';
 
-const image = document.createElement('img');
-image.src = jsLogo;
-image.style.maxWidth = '200px'; 
+const logo = new Image();
+logo.src = jsLogo;
+logo.style.maxWidth = '200px'; 
 
-document.body.appendChild(image);
+document.body.appendChild(logo);
 
 console.log('This is a webpack template');
 
